712a8b0cc848028c27c5d0c7707f1fe0a0afe38e
deshmukhprasad <deshmukhprasad@users.noreply.github.com>
2020-01-22 06:19:49 +0000

    Deploy

---
+++ precache-manifest.71e4c0c82f55783608c2e02e068ff725.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2734fa4c3c10c168eb49",
+    "url": "css/app.7d27ddc0.css"
+  },
+  {
+    "revision": "2734fa4c3c10c168eb49",
+    "url": "js/app.5ea03c86.js"
+  },
+  {
+    "revision": "c00aa80d34b334474459",
+    "url": "js/chunk-vendors.2d1eee83.js"
+  },
+  {
+    "revision": "5d75f2efa56b782ea7474f4b28600c64",
+    "url": "index.html"
+  },
+  {
+    "revision": "2b9cb912b95c35649da76ac7e3f9ce25",
+    "url": "img/opengraph.png"
+  },
+  {
+    "revision": "50d8a018e8ae96732c8a2ba663c61d4e",
+    "url": "robots.txt"
+  }
+];
